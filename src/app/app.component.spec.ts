b2a1cfa54689771022c02476b21e32172fc776be
Nour Krichene <krichenenour2@gmail.com>
2024-02-18 12:43:43 +0100

    init project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'saray-ui' title`, () => {
+  it(`should have the 'my-office-ui' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('saray-ui');
+    expect(app.title).toEqual('my-office-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, saray-ui');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-office-ui');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Task } from './Task';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'saray-ui';
+
+export class AppComponent implements OnInit {
+  tasks: Task[] = [];
+  addTaskForm!: FormGroup;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.getTasks();
+    this.addTaskForm = new FormGroup({
+      name: new FormControl(''),
+      description: new FormControl(''),
+      status: new FormControl('NOT_DONE')
+    });
+  }
+
+  getTasks(): void {
+    this.http.get<Task[]>('http://localhost:8081/tasks')
+      .subscribe(tasks => this.tasks = tasks);
+  }
+
+  addTask(): void {
+    const task: Task = this.addTaskForm.value;
+    task.creationDate = new Date();
+    console.log(task);
+    this.http.post<Task>('http://localhost:8081/task', task)
+      .subscribe(task => {
+        this.tasks.push(task);
+        this.addTaskForm.reset();
+      });
+  }
+
+  deleteTask(task: Task): void {
+    this.http.delete<Task>('http://localhost:8081/task/' + task.id)
+      .subscribe(() => {
+        this.tasks = this.tasks.filter(t => t.id !== task.id);
+      });
+  }
 }
