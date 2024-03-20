97f37389c695b7f3cd1c78678bd49ae04f6fb86e
Nour Krichene <krichenenour2@gmail.com>
2024-03-20 09:59:51 +0100

    Replace template-driven forms in add-task-modal and edit-task-modal with reactive forms

--- src/app/add-task-modal/add-task-modal.component.ts
+++ src/app/add-task-modal/add-task-modal.component.ts
@@ -1,21 +1,41 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { Task } from '../Task';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'add-task-modal',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './add-task-modal.component.html',
   styleUrl: './add-task-modal.component.css'
 })
-export class AddTaskModalComponent {
+export class AddTaskModalComponent implements OnInit {
   @Output() taskAdded = new EventEmitter<Task>();
-  task: Task = new Task('', '', '', 'NOT_DONE', new Date(), 0);
+  addTaskForm: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.addTaskForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['']
+    });
+  }
 
   submit(): void {
-    this.taskAdded.emit(this.task);
+    if (this.addTaskForm.valid) {
+      this.taskAdded.emit(
+        new Task(
+          '',
+          this.addTaskForm.value.name,
+          this.addTaskForm.value.description,
+          'NOT_DONE',
+          new Date(),
+          this.addTaskForm.value.priority
+        )
+      );
+    }
+
   }
 
 }
--- src/app/edit-task-modal/edit-task-modal.component.ts
+++ src/app/edit-task-modal/edit-task-modal.component.ts
@@ -1,26 +1,46 @@
-import { Component, Inject, Output, EventEmitter } from '@angular/core';
+import { Component, Inject, Output, EventEmitter, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Task } from '../Task';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'edit-task-modal',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './edit-task-modal.component.html',
   styleUrl: './edit-task-modal.component.css'
 })
-export class EditTaskModalComponent {
+export class EditTaskModalComponent implements OnInit {
   @Output() taskEdited = new EventEmitter<Task>();
   @Output() taskArchived = new EventEmitter<Task>();
   editedTask: Task;
+  editTaskForm: FormGroup = new FormGroup({});
 
-  constructor(@Inject(MAT_DIALOG_DATA) public task: Task) {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public task: Task, private formBuilder: FormBuilder) {
     this.editedTask = new Task(task.id, task.name, task.description, task.status, task.creationDate, task.priority);
   }
 
+  ngOnInit(): void {
+    this.editTaskForm = this.formBuilder.group({
+      name: [this.editedTask.name, Validators.required],
+      description: [this.editedTask.description],
+    });
+  }
+
   confirmEdit(): void {
-    this.taskEdited.emit(this.editedTask);
+    if (this.editTaskForm.valid && this.editTaskForm.dirty) {
+      this.taskEdited.emit(
+        new Task(
+          this.editedTask.id,
+          this.editTaskForm.value.name,
+          this.editTaskForm.value.description,
+          this.editedTask.status,
+          this.editedTask.creationDate,
+          this.editedTask.priority
+        )
+      );
+    }
   }
 
   archive(): void {
