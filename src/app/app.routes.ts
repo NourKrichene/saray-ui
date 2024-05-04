8691a49c9539a39e5938d02c4e051167d17ae5d0
Nour Krichene <krichenenour2@gmail.com>
2024-05-04 14:55:52 +0200

    Add welcome component and update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: WelcomeComponent },
+    { path: 'welcome', component: WelcomeComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: '**', redirectTo: '' }
+];
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,127 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+
+import {
+  DragDropModule,
+  CdkDragDrop,
+  CdkDropList
+} from '@angular/cdk/drag-drop';
+import { KeycloakService } from 'keycloak-angular';
+import { TaskCardComponent } from '../task-card/task-card.component';
+import { TaskService } from '../task.service';
+import { Task } from '../Task';
+import { AddTaskModalComponent } from '../add-task-modal/add-task-modal.component';
+import { EditTaskModalComponent } from '../edit-task-modal/edit-task-modal.component';
+import { TasksStore } from '../task.store';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [DragDropModule, CdkDropList, CommonModule, TaskCardComponent, RouterLink],
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+  providers: [TaskService]
+})
+export class DashboardComponent implements OnInit {
+  tasksToDo: Task[] = [];
+  tasksInProgress: Task[] = [];
+  tasksDone: Task[] = [];
+  loading = true;
+  loggedIn = false;
+
+  constructor(
+    private dialog: MatDialog,
+    private tasksStore: TasksStore,
+    private keycloakService: KeycloakService
+  ) { }
+
+  ngOnInit() {
+    this.getTasks();
+    this.loggedIn = this.keycloakService.isLoggedIn();
+  }
+
+  getTasks(): void {
+    this.tasksStore.getTasks().subscribe(({ toDo, inProgress, done }) => {
+      this.tasksToDo = toDo;
+      this.tasksInProgress = inProgress;
+      this.tasksDone = done;
+      this.loading = false;
+    });
+  }
+
+  openEditTaskModal(task: Task): void {
+    const dialogRef = this.dialog.open(EditTaskModalComponent, {
+      height: '380px',
+      width: '420px',
+      data: task
+    });
+
+    dialogRef.componentInstance.taskEdited.subscribe((editedTask: Task) => {
+      this.tasksStore.updateTask(editedTask);
+      dialogRef.close();
+    });
+
+    dialogRef.componentInstance.taskArchived.subscribe((editedTask: Task) => {
+      this.tasksStore.deleteTask(editedTask);
+      dialogRef.close();
+    });
+  }
+
+  openAddTaskModal(): void {
+    const dialogRef = this.dialog.open(AddTaskModalComponent, {
+      height: '360px',
+      width: '400px'
+    });
+
+    dialogRef.componentInstance.taskAdded.subscribe((task: Task) => {
+      this.tasksStore.addTask(task, this.tasksToDo.length);
+      dialogRef.close();
+    });
+  }
+
+  drop(event: CdkDragDrop<Task[]>, newStatus: string) {
+    if (event.previousContainer === event.container) {
+      const task = event.container.data[event.previousIndex];
+      task.priority = event.currentIndex;
+      this.tasksStore.updateTaskPriority(task, event.previousIndex);
+    } else {
+      const task = event.previousContainer.data[event.previousIndex];
+      const previousStatus = task.status;
+      const newContainerLength = event.container.data.length;
+      task.status = newStatus;
+      task.priority = event.currentIndex;
+      this.tasksStore.updateTaskPriorityAndStatus(task, previousStatus, newContainerLength);
+    }
+  }
+
+  filter(event: Event) {
+    const filterWord = (event.target as HTMLInputElement).value.toLowerCase();
+    this.tasksToDo = this.tasksToDo.filter(x => x.status === 'NOT_DONE' && x.name.toLowerCase().includes(filterWord));
+    this.tasksInProgress = this.tasksInProgress.filter(x => x.status === 'IN_PROGRESS' && x.name.toLowerCase().includes(filterWord));
+    this.tasksDone = this.tasksDone.filter(x => x.status === 'DONE' && x.name.toLowerCase().includes(filterWord));
+  }
+
+  login() {
+    this.keycloakService.login().then(() => {
+      this.loggedIn = true;
+    }
+    );
+  }
+  logout() {
+    this.keycloakService.logout().then(() => {
+      this.loggedIn = false;
+    }
+    );
+  }
+
+  saveClient() {
+    this.keycloakService.loadUserProfile().then((profile) => {
+      console.log("id : " + profile.id);
+      console.log("email : " + profile.email);
+    }
+    );
+  }
+
+}
